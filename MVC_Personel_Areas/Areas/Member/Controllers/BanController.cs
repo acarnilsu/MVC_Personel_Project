838fda711d340510cb7070db7463d74111480f17
nilsu <nilsu@DESKTOP-O6Q5UAT>
2022-09-12 00:07:28 +0300

    yess all task completed

---
+++ MVC_Personel_Areas/Areas/Member/Controllers/BanController.cs
@@ -0,0 +1,27 @@
+﻿using MVC_Personel_Areas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Personel_Areas.Areas.Member.Controllers
+{
+    public class BanController : Controller
+    {
+        // GET: Member/Ban
+
+        DbistanbulAkademiMvcEntities db = new DbistanbulAkademiMvcEntities();
+        public ActionResult Index(int id)
+        {
+            var member = db.TblMember.Find(id);
+            var banTimeHour = (Convert.ToDateTime(member.LockoutEnd).Subtract(DateTime.Now)).Hours;
+            var banTimeDay = (Convert.ToDateTime(member.LockoutEnd).Subtract(DateTime.Now)).Days;
+            var banTimeMinute = (Convert.ToDateTime(member.LockoutEnd).Subtract(DateTime.Now)).Minutes;
+            ViewBag.banTimeMinute = banTimeMinute;
+            ViewBag.banTimeHour = banTimeHour;
+            ViewBag.banTimeDay = banTimeDay;
+            return View();
+        }
+    }
+}
--- MVC_Personel_Areas/Areas/Member/Controllers/LoginController.cs
+++ MVC_Personel_Areas/Areas/Member/Controllers/LoginController.cs
@@ -24,16 +24,58 @@ namespace MVC_Personel_Areas.Areas.Member.Controllers
         public ActionResult Index(TblMember p)
         {
             var values = db.TblMember.FirstOrDefault(x => x.MemberMail == p.MemberMail && x.MemberPassword == p.MemberPassword);
-            if (values != null)
+            var selectUser = db.TblMember.FirstOrDefault(x => x.MemberMail == p.MemberMail);
+            if (values != null && values.LockoutEnabled==true)
             {
                 FormsAuthentication.SetAuthCookie(values.MemberMail, false);
                 Session["MemberMail"]=values.MemberMail;
                 return RedirectToAction("Index", "Profile");
             }
+            else if (values != null && values.LockoutEnabled==false)
+            {
+                var banTimeMinute = (Convert.ToDateTime(p.LockoutEnd).Subtract(DateTime.Now)).Minutes;
+                var banTimeHour = (Convert.ToDateTime(p.LockoutEnd).Subtract(DateTime.Now)).Hours;
+                var banTimeDay = (Convert.ToDateTime(p.LockoutEnd).Subtract(DateTime.Now)).Days;
+                if (banTimeMinute<=0 && banTimeHour<=0 && banTimeDay<=0)
+                {
+                    values.LockoutEnabled = true;
+                    values.LockoutEnd = null;
+                    values.AccessFailedCount = 0;
+                    db.SaveChanges();
+                    FormsAuthentication.SetAuthCookie(values.MemberMail, false);
+                    Session["MemberMail"] = values.MemberMail;
+                    return RedirectToAction("Index", "Profile");
+                }
+                else
+                {
+                    return RedirectToAction("Index/" + values.MemberID, "Ban");
+                }
+            }
             else
             {
-                return RedirectToAction("Index", "Login");
+                
+                if (selectUser?.AccessFailedCount < 3)
+                {
+                    selectUser.AccessFailedCount += 1;
+                    db.SaveChanges();
+                    return RedirectToAction("Index", "Login");
+                }
+                else
+                {
+                    selectUser.LockoutEnabled = false;
+                    DateTime date = DateTime.Now;
+                    TimeSpan banTime = new TimeSpan(3, 0, 0, 0);
+                    var banTimeEnd = date.Add(banTime);
+                    selectUser.LockoutEnd = banTimeEnd;
+                    db.SaveChanges();
+                    return RedirectToAction("Index/" + selectUser.MemberID, "Ban");
+                }
             }
+        }
+
+        public ActionResult LogOut()
+        {
+            return RedirectToAction("Index");
         }
     }
 }
